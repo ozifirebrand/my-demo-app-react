f013561c53a33a45bcca16d1b6854e24a49efb4e
Ozioma <oziomaokoroafor@gmail.com>
2022-01-17 18:14:32 +0100

    My App commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Learn React with Ozi
+        </p>
+    </div>
+  );
+}
+
+export default App;
